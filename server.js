ceb1772131011a7fed1170192dce6312ac0afd66
srodriguez1357 <chavarrieta.2000@gmail.com>
2022-11-12 23:24:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJwYXNzd29yZCI6ImFkaGR1ZW5zZiIsImFkbWluX2FjY2VzcyI6ZmFsc2V9.cikRAPMY7Bqx5jnQF2hKsvOq7kmatF_YsmUbX6Qz-JE"
 aws_token = "arn:aws:iam::123456789012:mfa/user"
 password= "124hsy3731"
