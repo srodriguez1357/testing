5e9eea1e43555d4f555b77047cac411ff31e140f
srodriguez1357 <chavarrieta.2000@gmail.com>
2022-11-12 23:20:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
-arn:aws:iam::123456789012:mfa/user
-password=124hsy3731
+jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+aws_token = "arn:aws:iam::123456789012:mfa/user"
+password= "124hsy3731"
